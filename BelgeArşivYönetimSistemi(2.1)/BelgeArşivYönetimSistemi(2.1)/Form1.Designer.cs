578bf6ceda2eb68e4b21062fa4ddbc796bbe7a1d
fatmanuryilmaz <fyilmz@icloud.com>
2023-06-16 17:40:48 +0300

    giriş ekranı yönetici ekranı
    giriş ve yönetici ekranı

---
+++ BelgeArşivYönetimSistemi(2.1)/BelgeArşivYönetimSistemi(2.1)/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace BelgeArşivYönetimSistemi_2._1_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BaslikLbl = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.KullaniciAdiLbl = new System.Windows.Forms.Label();
+            this.SifreLbl = new System.Windows.Forms.Label();
+            this.YetkiLbl = new System.Windows.Forms.Label();
+            this.SifreTxtbx = new System.Windows.Forms.TextBox();
+            this.KullaniciAdiTxtbx = new System.Windows.Forms.TextBox();
+            this.YoneticiRdioBttn = new System.Windows.Forms.RadioButton();
+            this.KullaniciRadioBttn = new System.Windows.Forms.RadioButton();
+            this.CikisBttn = new System.Windows.Forms.Button();
+            this.GirisBttn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BaslikLbl
+            // 
+            this.BaslikLbl.AutoSize = true;
+            this.BaslikLbl.Font = new System.Drawing.Font("Constantia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BaslikLbl.Location = new System.Drawing.Point(283, 65);
+            this.BaslikLbl.Name = "BaslikLbl";
+            this.BaslikLbl.Size = new System.Drawing.Size(265, 35);
+            this.BaslikLbl.TabIndex = 0;
+            this.BaslikLbl.Text = "Belge Arşiv Sistemi";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(54, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 114);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // KullaniciAdiLbl
+            // 
+            this.KullaniciAdiLbl.AutoSize = true;
+            this.KullaniciAdiLbl.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.KullaniciAdiLbl.Location = new System.Drawing.Point(312, 181);
+            this.KullaniciAdiLbl.Name = "KullaniciAdiLbl";
+            this.KullaniciAdiLbl.Size = new System.Drawing.Size(139, 24);
+            this.KullaniciAdiLbl.TabIndex = 2;
+            this.KullaniciAdiLbl.Text = "Kullanıcı Adı : ";
+            // 
+            // SifreLbl
+            // 
+            this.SifreLbl.AutoSize = true;
+            this.SifreLbl.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SifreLbl.Location = new System.Drawing.Point(386, 238);
+            this.SifreLbl.Name = "SifreLbl";
+            this.SifreLbl.Size = new System.Drawing.Size(65, 24);
+            this.SifreLbl.TabIndex = 2;
+            this.SifreLbl.Text = "Şifre : ";
+            // 
+            // YetkiLbl
+            // 
+            this.YetkiLbl.AutoSize = true;
+            this.YetkiLbl.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.YetkiLbl.Location = new System.Drawing.Point(382, 290);
+            this.YetkiLbl.Name = "YetkiLbl";
+            this.YetkiLbl.Size = new System.Drawing.Size(69, 24);
+            this.YetkiLbl.TabIndex = 2;
+            this.YetkiLbl.Text = "Yetki : ";
+            // 
+            // SifreTxtbx
+            // 
+            this.SifreTxtbx.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.SifreTxtbx.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SifreTxtbx.Location = new System.Drawing.Point(457, 230);
+            this.SifreTxtbx.Name = "SifreTxtbx";
+            this.SifreTxtbx.Size = new System.Drawing.Size(212, 32);
+            this.SifreTxtbx.TabIndex = 3;
+            // 
+            // KullaniciAdiTxtbx
+            // 
+            this.KullaniciAdiTxtbx.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.KullaniciAdiTxtbx.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.KullaniciAdiTxtbx.Location = new System.Drawing.Point(457, 173);
+            this.KullaniciAdiTxtbx.Name = "KullaniciAdiTxtbx";
+            this.KullaniciAdiTxtbx.Size = new System.Drawing.Size(212, 32);
+            this.KullaniciAdiTxtbx.TabIndex = 3;
+            // 
+            // YoneticiRdioBttn
+            // 
+            this.YoneticiRdioBttn.AutoSize = true;
+            this.YoneticiRdioBttn.BackColor = System.Drawing.Color.Transparent;
+            this.YoneticiRdioBttn.Font = new System.Drawing.Font("Constantia", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.YoneticiRdioBttn.Location = new System.Drawing.Point(470, 291);
+            this.YoneticiRdioBttn.Name = "YoneticiRdioBttn";
+            this.YoneticiRdioBttn.Size = new System.Drawing.Size(91, 25);
+            this.YoneticiRdioBttn.TabIndex = 4;
+            this.YoneticiRdioBttn.TabStop = true;
+            this.YoneticiRdioBttn.Text = "Yönetici";
+            this.YoneticiRdioBttn.UseVisualStyleBackColor = false;
+            // 
+            // KullaniciRadioBttn
+            // 
+            this.KullaniciRadioBttn.AutoSize = true;
+            this.KullaniciRadioBttn.BackColor = System.Drawing.Color.Transparent;
+            this.KullaniciRadioBttn.Font = new System.Drawing.Font("Constantia", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.KullaniciRadioBttn.Location = new System.Drawing.Point(571, 291);
+            this.KullaniciRadioBttn.Name = "KullaniciRadioBttn";
+            this.KullaniciRadioBttn.Size = new System.Drawing.Size(98, 25);
+            this.KullaniciRadioBttn.TabIndex = 4;
+            this.KullaniciRadioBttn.TabStop = true;
+            this.KullaniciRadioBttn.Text = "Kullanıcı";
+            this.KullaniciRadioBttn.UseVisualStyleBackColor = false;
+            // 
+            // CikisBttn
+            // 
+            this.CikisBttn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.CikisBttn.Font = new System.Drawing.Font("Constantia", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CikisBttn.Location = new System.Drawing.Point(566, 352);
+            this.CikisBttn.Name = "CikisBttn";
+            this.CikisBttn.Size = new System.Drawing.Size(103, 49);
+            this.CikisBttn.TabIndex = 5;
+            this.CikisBttn.Text = "Çıkış Yap";
+            this.CikisBttn.UseVisualStyleBackColor = false;
+            // 
+            // GirisBttn
+            // 
+            this.GirisBttn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.GirisBttn.Font = new System.Drawing.Font("Constantia", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.GirisBttn.Location = new System.Drawing.Point(415, 352);
+            this.GirisBttn.Name = "GirisBttn";
+            this.GirisBttn.Size = new System.Drawing.Size(103, 49);
+            this.GirisBttn.TabIndex = 5;
+            this.GirisBttn.Text = "Giriş Yap";
+            this.GirisBttn.UseVisualStyleBackColor = false;
+            this.GirisBttn.Click += new System.EventHandler(this.GirisBttn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GirisBttn);
+            this.Controls.Add(this.CikisBttn);
+            this.Controls.Add(this.KullaniciRadioBttn);
+            this.Controls.Add(this.YoneticiRdioBttn);
+            this.Controls.Add(this.KullaniciAdiTxtbx);
+            this.Controls.Add(this.SifreTxtbx);
+            this.Controls.Add(this.YetkiLbl);
+            this.Controls.Add(this.SifreLbl);
+            this.Controls.Add(this.KullaniciAdiLbl);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BaslikLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label BaslikLbl;
+        private PictureBox pictureBox1;
+        private Label KullaniciAdiLbl;
+        private Label SifreLbl;
+        private Label YetkiLbl;
+        private TextBox SifreTxtbx;
+        private TextBox KullaniciAdiTxtbx;
+        private RadioButton YoneticiRdioBttn;
+        private RadioButton KullaniciRadioBttn;
+        private Button CikisBttn;
+        private Button GirisBttn;
+    }
+}
---
+++ BelgeArşivYönetimSistemi(2.1)/BelgeArşivYönetimSistemi(2.1)/Form1.cs
@@ -0,0 +1,113 @@
+using MySql.Data.MySqlClient;
+namespace BelgeArşivYönetimSistemi_2._1_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string mysqlCon = "server=172.21.54.3; user=Grup2-2023; database=Grup2-2023; password=NyP:974.cc";
+            MySqlConnection mySqlConnection =new MySqlConnection(mysqlCon);
+            try
+            {
+                mySqlConnection.Open();
+                MessageBox.Show("Bağlantı Kuruldu.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                mySqlConnection.Close();
+            }
+        }
+        //Formlar arası veri tanımlama
+        public static string id, adi, soyadi, yetki;
+        //Yerel değişken tanımlama
+        int hak = 3; bool durum = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = " Kullanıcı Girişi ";
+            this.AcceptButton = GirisBttn; this.CancelButton = CikisBttn;
+            YoneticiRdioBttn.Checked = true;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+
+        }
+
+        private void GirisBttn_Click(object sender, EventArgs e)
+        {
+            if (hak != 0)
+            {
+                string mysqlCon = "server=172.21.54.3; user=Grup2-2023; database=Grup2-2023; password=NyP:974.cc";
+                MySqlConnection mySqlConnection = new MySqlConnection(mysqlCon);
+                mySqlConnection.Open();
+
+                MySqlCommand selectsorgu=new MySqlCommand("select * from Kullanici",mySqlConnection);
+                MySqlDataReader kayitOkuma=selectsorgu.ExecuteReader();
+                
+                while (kayitOkuma.Read())
+                {
+                    if (YoneticiRdioBttn.Checked == true)
+                    {
+                        if (kayitOkuma["kullaniciAd"].ToString()== KullaniciAdiTxtbx.Text && kayitOkuma["sifre"].ToString()== SifreTxtbx.Text && kayitOkuma["yetki"].ToString() == "Yönetici")
+                         {
+
+                            durum = true;
+                            id = kayitOkuma.GetValue(0).ToString();
+                            adi = kayitOkuma.GetValue(1).ToString();
+                            soyadi = kayitOkuma.GetValue(2).ToString();
+                            yetki = kayitOkuma.GetValue(3).ToString();
+                            this.Hide();
+                            Form2 frm2=new Form2();
+                            frm2.Show();
+                            break;
+
+
+
+                         }
+                                
+                    }
+                    if(KullaniciRadioBttn.Checked == true)
+                    {
+                        if (kayitOkuma["kullaniciAd"].ToString() == KullaniciAdiTxtbx.Text && kayitOkuma["sifre"].ToString() == SifreTxtbx.Text && kayitOkuma["yetki"].ToString() == "Kullanıcı")
+                        {
+
+                            durum = true;
+                            id = kayitOkuma.GetValue(0).ToString();
+                            adi = kayitOkuma.GetValue(1).ToString();
+                            soyadi = kayitOkuma.GetValue(2).ToString();
+                            yetki = kayitOkuma.GetValue(3).ToString();
+                            this.Hide();
+                            Form3 frm3 = new Form3();
+                            frm3.Show();
+                            break;
+
+
+
+                        }
+
+                    }
+
+
+                }
+                if (durum == false)
+                
+                    hak--;
+                 mySqlConnection.Close();
+
+            }
+            if (hak == 0)
+            {
+                GirisBttn.Enabled = false;
+                MessageBox.Show("Giriş Hakkınız Dolmuştur.","Belge Arşiv Yönetim Sistemi",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                this.Close();
+            }
+        }
+
+       
+
+    }
+}
